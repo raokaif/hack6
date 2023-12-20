84cccf63341b72607214563ee145a8be76082a30
raokaif <kaifrao250@gmail.com>
2023-12-20 20:34:01 +0500

    using formulas to convert rgb to cmyk

--- colorUtils.c
+++ colorUtils.c
@@ -1,8 +1,26 @@
-#include<stdio.h>
-int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k){
+#include <stdio.h>
+int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
+{
     int r2, g2, b2;
-    r2=r/255;
-    g2 = g/ 255;
-    b2 = b/255; 
-    
+    int c1, m1, y1, k1;
+    int max;
+    if (r >= 0 && r <= 255)
+    {
+        r2 = r / 255;
+        g2 = g / 255;
+        b2 = b / 255;
+
+        max = r2 > g2 ? r2 : g2;
+        max = max > b2 ? max : b2;
+        k1 = 1 - max;
+
+        c1 = ((1 - r2 - k1) / (1 - k1));
+        m1 = ((1 - b2 - k1) / (1 - k1));
+        y1 = ((1 - g2 - k1) / (1 - k1));
+        return 0;
+    }
+    else
+    {
+        return 1;
+    }
 }
