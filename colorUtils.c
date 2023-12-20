298d2eacd810bb7cc072260021abdedae5084170
raokaif <kaifrao250@gmail.com>
2023-12-20 20:35:59 +0500

    if for checking error code

--- colorUtils.c
+++ colorUtils.c
@@ -4,7 +4,7 @@ int rgbToCMYK(int r, int g, int b, double *c, double *m, double *y, double *k)
     int r2, g2, b2;
     int c1, m1, y1, k1;
     int max;
-    if (r >= 0 && r <= 255)
+    if (r >= 0 && r <= 255 && g >= 0 && g <= 255 && b >= 0 && b <= 255)
     {
         r2 = r / 255;
         g2 = g / 255;
